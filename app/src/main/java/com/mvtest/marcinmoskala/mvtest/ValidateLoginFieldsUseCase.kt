cc2fdbb0ec67a8f5f1c7c94fd515288a43d3c7ab
Marcin Moskala <marcinmoskala@gmail.com>
2017-03-28 09:42:49 +0200

    Fix in password validation

--- app/src/main/java/com/mvtest/marcinmoskala/mvtest/ValidateLoginFieldsUseCase.kt
+++ app/src/main/java/com/mvtest/marcinmoskala/mvtest/ValidateLoginFieldsUseCase.kt
@@ -10,19 +10,19 @@ class ValidateLoginFieldsUseCase {
 
     private fun getEmailErrorId(email: String) = when {
         email.isEmpty() -> R.string.error_field_required
-        !isEmailValid(email) -> R.string.error_invalid_email
+        emailInvalid(email) -> R.string.error_invalid_email
         else -> null
     }
 
     private fun getPasswordErrorId(password: String) = when {
         password.isEmpty() -> R.string.error_field_required
-        isPasswordValid(password) -> R.string.error_invalid_password
+        passwordInvalid(password) -> R.string.error_invalid_password
         else -> null
     }
 
-    private fun isEmailValid(email: String): Boolean = email.contains("@")
+    private fun emailInvalid(email: String): Boolean = !email.contains("@")
 
-    private fun isPasswordValid(password: String): Boolean = password.length > 4
+    private fun passwordInvalid(password: String): Boolean = password.length <= 4
 
     data class LoginErrors(val emailErrorId: Int? = null, val passwordErrorId: Int? = null) {
         val correct get() = emailErrorId == null && passwordErrorId == null
