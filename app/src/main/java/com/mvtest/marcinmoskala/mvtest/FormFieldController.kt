e269a738294b7a110a6cbee75084169d8f52d62b
Marcin Moskala <marcinmoskala@gmail.com>
2017-03-28 09:19:58 +0200

    Improvement

--- app/src/main/java/com/mvtest/marcinmoskala/mvtest/FormFieldController.kt
+++ app/src/main/java/com/mvtest/marcinmoskala/mvtest/FormFieldController.kt
@@ -8,19 +8,21 @@ class EmailController : FormFieldController {
 
     override fun getErrorId(text: String) = when {
         text.isEmpty() -> R.string.error_field_required
-        !isEmailValid(text) -> R.string.error_invalid_email
+        emailInvalid(text) -> R.string.error_invalid_email
         else -> null
     }
 
-    private fun isEmailValid(email: String): Boolean = email.contains("@")
+    private fun emailInvalid(email: String): Boolean = !email.contains("@")
 }
 
 class PasswordController : FormFieldController {
 
     override fun getErrorId(text: String) = when {
-        text.isEmpty() || isPasswordValid(text) -> R.string.error_invalid_password
+        text.isEmpty() -> R.string.error_field_required
+        passwordInvalid(text) -> R.string.error_invalid_password
         else -> null
     }
 
-    private fun isPasswordValid(password: String): Boolean = password.length > 4
+    private fun passwordInvalid(password: String): Boolean = password.length <= 4
 }
+
