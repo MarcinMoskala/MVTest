9c59b7d158960c22c16b84b0cceba33e0433d0fd
Marcin Moskala <marcinmoskala@gmail.com>
2017-03-28 09:22:38 +0200

    Improvement

--- app/src/main/java/com/mvtest/marcinmoskala/mvtest/LoginController.kt
+++ app/src/main/java/com/mvtest/marcinmoskala/mvtest/LoginController.kt
@@ -3,57 +3,58 @@ package com.mvtest.marcinmoskala.mvtest
 import android.widget.EditText
 import rx.Subscription
 
-    class LoginController(val view: LoginActivity, val emailView: EditText, val passwordView: EditText) {
+class LoginController(val view: LoginActivity, val emailView: EditText, val passwordView: EditText) {
 
-        val loginRepository by LoginRepository.lazyGet()
-        var subscriptions: List<Subscription> = emptyList()
+    val loginRepository by LoginRepository.lazyGet()
+    var subscriptions: List<Subscription> = emptyList()
 
-        fun onDestroy() {
-            subscriptions.forEach { it.unsubscribe() }
-        }
-
-        fun attemptLogin() {
-            val email = emailView.text.toString()
-            val password = passwordView.text.toString()
-
-            val passwordErrorId = getPasswordErrorId(password)
-            passwordView.setErrorId(passwordErrorId)
-
-            val emailErrorId = getEmailErrorId(email)
-            emailView.setErrorId(emailErrorId)
-
-            when {
-                emailErrorId != null -> emailView.requestFocus()
-                passwordErrorId != null -> passwordView.requestFocus()
-                else -> sendLoginRequest(email, password)
-            }
-        }
-
-        private fun sendLoginRequest(email: String, password: String) {
-            subscriptions += loginRepository.attemptLogin(email, password)
-                    .applySchedulers()
-                    .smartSubscribe(
-                            onStart = { view.showProgress(true) },
-                            onSuccess = { (token) -> view.toast("Login succeed. Token: $token") },
-                            onError = { view.toast("Error occurred") },
-                            onFinish = { view.showProgress(false) }
-                    )
-        }
-
-        private fun getEmailErrorId(email: String) = when {
-            email.isEmpty() -> R.string.error_field_required
-            !isEmailValid(email) -> R.string.error_invalid_email
-            else -> null
-        }
-
-        private fun getPasswordErrorId(email: String) = when {
-            email.isEmpty() || isPasswordValid(email) -> R.string.error_invalid_password
-            else -> null
-        }
-
-        private fun isEmailValid(email: String): Boolean = email.contains("@")
-
-        private fun isPasswordValid(password: String): Boolean = password.length > 4
+    fun onDestroy() {
+        subscriptions.forEach { it.unsubscribe() }
     }
+
+    fun attemptLogin() {
+        val email = emailView.text.toString()
+        val password = passwordView.text.toString()
+
+        val passwordErrorId = getPasswordErrorId(password)
+        passwordView.setErrorId(passwordErrorId)
+
+        val emailErrorId = getEmailErrorId(email)
+        emailView.setErrorId(emailErrorId)
+
+        when {
+            emailErrorId != null -> emailView.requestFocus()
+            passwordErrorId != null -> passwordView.requestFocus()
+            else -> sendLoginRequest(email, password)
+        }
+    }
+
+    private fun sendLoginRequest(email: String, password: String) {
+        subscriptions += loginRepository.attemptLogin(email, password)
+                .applySchedulers()
+                .smartSubscribe(
+                        onStart = { view.showProgress(true) },
+                        onSuccess = { (token) -> view.toast("Login succeed. Token: $token") },
+                        onError = { view.toast("Error occurred") },
+                        onFinish = { view.showProgress(false) }
+                )
+    }
+
+    private fun getEmailErrorId(email: String) = when {
+        email.isEmpty() -> R.string.error_field_required
+        emailInvalid(email) -> R.string.error_invalid_email
+        else -> null
+    }
+
+    private fun getPasswordErrorId(email: String) = when {
+        email.isEmpty() -> R.string.error_field_required
+        passwordInvalid(email) -> R.string.error_invalid_password
+        else -> null
+    }
+
+    private fun emailInvalid(email: String): Boolean = !email.contains("@")
+
+    private fun passwordInvalid(password: String): Boolean = password.length <= 4
+}
 
 
